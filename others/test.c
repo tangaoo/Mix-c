eb9b4009bc79a52e87e04968ae665222c323585e
tangaoo <tangaoo@126.com>
2020-12-03 20:15:06 +0800

    add sth

---
+++ others/test.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("hi\n");
+
+    return 0;
+}
