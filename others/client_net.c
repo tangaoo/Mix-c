2c5d3493c4050eb13936921dbb477a6291dc5dde
tangaoo <tangaoo@126.com>
2020-12-27 15:35:08 +0800

    test socket from 2 computer.

--- others/client_net.c
+++ others/client_net.c
@@ -13,8 +13,8 @@ int main()
     struct sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
     serv_addr.sin_family = AF_INET;  //使用IPv4地址
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  //具体的IP地址
-    serv_addr.sin_port = htons(1234);  //端口
+    serv_addr.sin_addr.s_addr = inet_addr("192.168.31.57");  //具体的IP地址
+    serv_addr.sin_port = htons(1235);  //端口
     connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
    
     //读取服务器传回的数据
